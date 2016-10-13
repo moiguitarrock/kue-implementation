744acc399ee2964924842bbd1727186ea2ea38ed
Moises Cuervo <maccma.c2@gmail.com>
2016-10-13 16:43:11 -0500

    Update test.config.js

--- config/test.config.js
+++ config/test.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  API_QUEUE_ENDPOINT : 'http://test.api.cebroker.com/v2/elasticsearchqueue',
-  API_BULK_POST: 'http://test.api.cebroker.com/v2/document/bulk',
+  API_QUEUE_ENDPOINT : 'http://some.endpoint.com',
+  API_BULK_POST: 'http://some.endpoint.com',
   REDIS_HOST: 'localhost',
   REDIS_PORT: 6379
 }
